c69726a0237b7fae6931cff7376b945917e5c0ff
paula-morales <pmpoclava@gmail.com>
2020-07-15 11:31:36 +0200

    new endpoint to get reviews by parkId

--- routers/park.js
+++ routers/park.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { Router } = express;
 const router = new Router();
 const Park = require("../models").park;
+const Review = require("../models").review;
 const authMiddleware = require("../auth/middleware");
 
 router.post("/", authMiddleware, async (req, res) => {
@@ -32,6 +33,19 @@ router.get("/", async (req, res, next) => {
   try {
     const parks = await Park.findAll();
     res.status(201).json(parks);
+  } catch (e) {
+    return res.status(400).send({ message: "Something went wrong, sorry" });
+  }
+});
+
+router.get("/:parkId/reviews", async (req, res, next) => {
+  const { parkId } = req.params;
+
+  try {
+    const getReviewByParkId = await Review.findAll({
+      where: { parkId },
+    });
+    res.status(201).json(getReviewByParkId);
   } catch (e) {
     return res.status(400).send({ message: "Something went wrong, sorry" });
   }
